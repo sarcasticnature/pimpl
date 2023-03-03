389eb53a53e05f402a2497d0c482ef2b260876d1
Jake Keller <jakeller3630@gmail.com>
2023-03-02 18:51:31 -0600

    Add tests for Sentence::evaluate()

--- src/sentence.cpp
+++ src/sentence.cpp
@@ -32,6 +32,9 @@ std::optional<bool> Sentence::evaluate(std::unordered_map<std::string, bool> sym
 
 std::optional<bool> Sentence::truth()
 {
+    if (data_ == nullptr) {
+        return std::nullopt;
+    }
     return std::visit(TruthVisitor{}, *data_);
 }
 
@@ -236,5 +239,103 @@ TEST_CASE("truth()")
             Sentence sentence(data, symbols);
             CHECK(sentence.truth() == true);
         }
+    }
+}
+
+TEST_CASE("evaluate()")
+{
+    using namespace pimpl;
+    using s_t = Sentence::sentence_t;
+
+    Sentence::sentence_ptr_t data;
+    std::unordered_map<std::string, Sentence::sentence_ptr_t> symbols;
+    std::unordered_map<std::string, bool> symbol_values;
+
+    // Tests for malformed inputs/sentence structure
+    SUBCASE("empty sentence") {
+        Sentence sentence;
+        REQUIRE(sentence.evaluate(symbol_values) == std::nullopt);
+    }
+    SUBCASE("missing/mismatched symbol values") {
+        data = std::make_shared<s_t>(Sentence::And(
+            std::make_shared<s_t>("foo"),
+            std::make_shared<s_t>("bar")
+        ));
+        symbols.insert({"foo", std::get<Sentence::And>(*data).left});
+        symbols.insert({"bar", std::get<Sentence::And>(*data).right});
+        Sentence sentence(data, symbols);
+
+        REQUIRE_MESSAGE(sentence.evaluate(symbol_values) == std::nullopt,
+                        "empty symbol_values");
+        symbol_values.insert({"foo", false});
+        REQUIRE_MESSAGE(sentence.evaluate(symbol_values) == std::nullopt,
+                        "too few symbol_values");
+        symbol_values.insert({"bar", true});
+        symbol_values.insert({"baz", true});
+        REQUIRE_MESSAGE(sentence.evaluate(symbol_values) == std::nullopt,
+                        "too many symbol_values");
+    }
+
+    SUBCASE("unexpected monostate") {
+        data = std::make_shared<s_t>(Sentence::Or(
+            std::make_shared<s_t>(std::monostate()),
+            std::make_shared<s_t>(true)
+        ));
+        Sentence sentence(data, symbols);
+        REQUIRE(sentence.evaluate(symbol_values) == std::nullopt);
+    }
+
+    SUBCASE("unexpected symbol") {
+        data = std::make_shared<s_t>(Sentence::Imp(
+            std::make_shared<s_t>("foo"),
+            std::make_shared<s_t>("bar")
+        ));
+        symbols.insert({"foo", std::get<Sentence::Imp>(*data).left});
+        Sentence sentence(data, symbols);
+        symbol_values.insert({"foo", true});
+        REQUIRE(sentence.evaluate(symbol_values) == std::nullopt);
+    }
+
+    // Tests for successful evaluation
+    SUBCASE("single true") {
+        data = std::make_shared<s_t>(true);
+        Sentence sentence(data, symbols);
+        REQUIRE(sentence.evaluate(symbol_values) == true);
+    }
+
+    SUBCASE("single false") {
+        data = std::make_shared<s_t>(false);
+        Sentence sentence(data, symbols);
+        REQUIRE(sentence.evaluate(symbol_values) == false);
+    }
+
+    SUBCASE("symbol-free tautology (F => T)") {
+        data = std::make_shared<s_t>(Sentence::Imp(
+            std::make_shared<s_t>(false),
+            std::make_shared<s_t>(true)
+        ));
+        Sentence sentence(data, symbols);
+        REQUIRE(sentence.evaluate(symbol_values) == true);
+    }
+
+    SUBCASE("single symbol") {
+        data = std::make_shared<s_t>("foo");
+        symbols.insert({"foo", data});
+        Sentence sentence(data, symbols);
+        symbol_values.insert({"foo", true});
+        REQUIRE(sentence.evaluate(symbol_values) == true);
+    }
+
+    SUBCASE("symbol substitution tautology (T & T)") {
+        data = std::make_shared<s_t>(Sentence::And(
+            std::make_shared<s_t>("foo"),
+            std::make_shared<s_t>("bar")
+        ));
+        symbols.insert({"foo", std::get<Sentence::And>(*data).left});
+        symbols.insert({"bar", std::get<Sentence::And>(*data).right});
+        Sentence sentence(data, symbols);
+        symbol_values.insert({"foo", true});
+        symbol_values.insert({"bar", true});
+        REQUIRE(sentence.evaluate(symbol_values) == true);
     }
 }
